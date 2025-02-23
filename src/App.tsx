ddbec372a7075945bbbb512488aef57806e4e6f6
Ved <garciaveduar@gmail.com>
2025-02-23 12:19:51 -0400

    Add: React-Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import Layout from "./components/Layout";
-import Greeting from "./components/Greeting";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
 
-function App() {
+
+const App: React.FC = () => {
   return (
-    <Layout>
-      <Greeting />
-    </Layout>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
   );
 }
 
